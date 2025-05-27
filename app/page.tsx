206722be676e61089a23bd85a976352a89382bd4
vulture02 <amithp0210@gmail.com>
2025-05-27 17:21:01 +0530

    dynamic error in page .tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from '@/components/ui/button'
+export const dynamic = "force-dynamic"; // ⬅️ Add this line
 
+import { Button } from '@/components/ui/button'
 import React from 'react'
 import CompanionsList from '@/components/CompanionsList'
 import CTA from '@/components/CTA'
 import CompanionCard from '@/components/CompanionCard'
-import { recentSessions } from '@/constants'
 import { getAllCompanions, getRecentSessions } from '@/lib/actions/companion.action'
 import { getSubjectColor } from '@/lib/utils'
 
@@ -12,31 +12,30 @@ const Page = async () => {
     const companions = await getAllCompanions({ limit: 3 });
     const recentSessionsCompanions = await getRecentSessions(10);
 
-  return (
-    <main>
-      <h1>Popular Companions</h1>
+    return (
+        <main>
+            <h1>Popular Companions</h1>
 
-        <section className="home-section">
-            {companions.map((companion) => (
-                <CompanionCard
-                    key={companion.id}
-                    {...companion}
-                    color={getSubjectColor(companion.subject)}
+            <section className="home-section">
+                {companions.map((companion) => (
+                    <CompanionCard
+                        key={companion.id}
+                        {...companion}
+                        color={getSubjectColor(companion.subject)}
+                    />
+                ))}
+            </section>
+
+            <section className="home-section">
+                <CompanionsList
+                    title="Recently completed sessions"
+                    companions={recentSessionsCompanions}
+                    classNames="w-2/3 max-lg:w-full"
                 />
-            ))}
-
-        </section>
-
-        <section className="home-section">
-            <CompanionsList
-                title="Recently completed sessions"
-                companions={recentSessionsCompanions}
-                classNames="w-2/3 max-lg:w-full"
-            />
-            <CTA />
-        </section>
-    </main>
-  )
+                <CTA />
+            </section>
+        </main>
+    )
 }
 
 export default Page
